132ae3f0a1c411afd5e8022bebd50ee2416d8ff9
Abhijeet Anand <abhijeetanand2011@gmail.com>
2023-11-07 22:09:19 -0800

    cleaned some functions

--- create_galaxy_archetype.py
+++ create_galaxy_archetype.py
@@ -1,4 +1,3 @@
-from desisim.templates import ELG, LRG, BGS
 from desispec.interpolation import resample_flux
 import numpy as np
 from astropy.io import fits
@@ -8,24 +7,19 @@ import fitsio
 import os
 import numba as nb
 import time
+from astropy.table import Table
 
-template_dir = '/global/cfs/cdirs/desi/spectro/templates/basis_templates/v3.0/'
+template_dir = '/global/cfs/cdirs/desicollab/users/ioannis/fastspecfit/redrock-templates/data/'
 
-def read_a_few_properties(subtype, keys, inds):
+def read_galaxy_template_properties(subtype):
     
-    if subtype in ['elg', 'lrg']:
-        filename = template_dir+'%s_templates_v2.1.fits'%(subtype)
-    else:
-        filename = template_dir+'%s_templates_v2.2.fits'%(subtype)
-        
+    filename = template_dir+f'ftemplates-iron-{subtype}.fits' 
     hdu = fits.open(filename)
-    data = hdu[1].data
+    metadata = hdu["METADATA"].data
+    flux = hdu["FLUX"].data
+    wave = hdu["WAVE"].data
     hdu.close()
-    props = {}
-    for key in keys:
-        props[key] = data[key][inds]
-    
-    return props
+    return metadata, flux, wave
 
 
 @nb.njit(fastmath=True, nopython=True)
@@ -61,7 +55,7 @@ def new_compute_chi2_numba(flux):
     
     return chi2, amp
         
-def generate_archetype_galaxies(nb=100, dw=0.1, chi2_thresh=10**2.5, file_out=None):  
+def generate_synthetic_galaxies(nb=1000, dw=0.1, file_out=None):  
 
     """ Generate synthetic spectra and run SetCoverPy to construct a set of archetypes 
     
@@ -69,17 +63,18 @@ def generate_archetype_galaxies(nb=100, dw=0.1, chi2_thresh=10**2.5, file_out=No
     ------------------------
         nb (int): number of spectra to be generated (default 1000)
         dw (float); wavelength pixel size (in Ang) (default 0.1)
-        chi2_thresh (float); chi2 square threshold that will be used for setcoverpy run (default 10**2.5)
         file_out (string); filename to save the data (.fits format)
 
     Output:
     ------------------------
         Returns a fits file containing the archetype wavelengt, fluxes and galaxy properties
     """
-    
+    start = time.time()
     if file_out is None:
         print('Provide filename to save the archetypes\n')
         return None
+    else:
+        print(f'synthetic galaxy spectra will be saved at {file_out}')
 
     ### Rest - frame wavelength definition
     wave = np.arange(3500./(1.+1.85), 11000.+dw/2., dw)
@@ -88,32 +83,34 @@ def generate_archetype_galaxies(nb=100, dw=0.1, chi2_thresh=10**2.5, file_out=No
     data = {}
 
     ### Galaxy properties
-    prop_keys = ['LOGMSTAR  ', 'LOGSFR  ', 'AV_ISM  ']
-
+    prop_keys = ['LOGMSTAR', 'LOGSSFR', 'AGE', 'ZZSUN', 'AV', 'VDISP', 'TARGETID']
+    units = ['Msun/yr', 'yr^-1', 'Gyr', '', 'mag', 'km/s', '']
     ### Galaxy types
-    all_subtypes = ['ELG', 'LRG', 'BGS']
+    all_subtypes = ['ELG', 'LRG', 'BGS_ANY']
 
     for subtype in all_subtypes:
         data[subtype] = {}
         data[subtype]['NB'] = int(1./3.*nb)
-    data['BGS']['NB'] = nb-data['ELG']['NB']-data['LRG']['NB']
+    data['BGS_ANY']['NB'] = nb-data['ELG']['NB']-data['LRG']['NB']
     data['ELG']['SEED'] = seed
     data['LRG']['SEED'] = seed+data['ELG']['NB']
-    data['BGS']['SEED'] = seed+data['ELG']['NB']+data['LRG']['NB']
+    data['BGS_ANY']['SEED'] = seed+data['ELG']['NB']+data['LRG']['NB']
 
    
-    ### Generating synthetic spectra
-
-    all_func = {'ELG':ELG(), 'LRG':LRG(), 'BGS':BGS()}
+    ### Selecting a small sample of synthetic spectra
 
     for subtype in all_subtypes:
-        data[subtype]['FLUX'], data[subtype]['WAVE'], data[subtype]['META'], data[subtype]['OBJMETA'] = all_func[subtype].make_templates(data[subtype]['NB'],restframe=True,nocolorcuts=True,seed=data[subtype]['SEED'])
-        props = read_a_few_properties(subtype.lower(), keys=prop_keys, inds =  data[subtype]['META']['TEMPLATEID'])
-        data[subtype]['LOGSSFR'] = props['LOGSFR  ']-props['LOGMSTAR  ']
-        data[subtype]['AV_ISM'] = props['AV_ISM  ']
-        data[subtype]['LOGMSTAR'] = props['LOGMSTAR  ']
-        data[subtype]['LOGSFR'] = props['LOGSFR  ']
-        data[subtype]['TEMPLATEID'] = np.array(['%s_%d'%(subtype, tt) for tt in data[subtype]['META']['TEMPLATEID']], dtype='U32')
+        props, tflux, twave = read_galaxy_template_properties(subtype)
+        rand = np.random.RandomState(seed=data[subtype]['SEED'])
+        inds = rand.choice(tflux.shape[0], size=data[subtype]['NB'],replace=False)
+        data[subtype]['FLUX']=tflux[inds]
+        data[subtype]['WAVE']=twave
+        logssfr = np.log10(props['SFR'][inds])-props['LOGMSTAR'][inds]
+        data[subtype]['LOGSSFR']=logssfr
+        data[subtype]['TEMPLATEID'] = inds
+        for ppkey in prop_keys:
+            if ppkey not in ['LOGSSFR']:
+                data[subtype][ppkey] = props[ppkey][inds]
     
     ### Combining all galaxies
     nTot = np.sum([ data[k]['NB'] for k in list(data.keys()) ])
@@ -132,25 +129,76 @@ def generate_archetype_galaxies(nb=100, dw=0.1, chi2_thresh=10**2.5, file_out=No
     flux /= np.median(flux,axis=1)[:,None]
     
     properties = {'FLUX':flux, 'SUBTYPE':subtype, 'TEMPLATEID':templateid}
-    save_keys1, save_keys2, save_keys3 = ['LOGSFR', 'LOGSSFR', 'LOGMSTAR', 'AV_ISM'], ['FLUX_R', 'FLUX_G', 'FLUX_Z'], ['VDISP']
-    
-    units = ['Msun/yr', 'yr^-1', 'Msun', 'mag', 'nanomaggies', 'nanomaggies', 'nanomaggies', 'km/s']
-    
-    ii = 0
-    for key in save_keys1:
-        key1 = key+'(%s)'%(units[ii])
+    final_keys = []
+    for kk in range(len(prop_keys)):
+        key = prop_keys[kk]
+        if key not in ['TARGETID', 'ZZSUN']:
+            key1 = key+'(%s)'%(units[kk])
+        else:
+            key1 = key
+        final_keys.append(key1)
         properties[key1] = np.concatenate([data[subtype][key] for subtype in all_subtypes])
-        ii=ii+1
-    for key in save_keys2:
-        key1 = key+'(%s)'%(units[ii])
-        properties[key1] = np.concatenate([data[subtype]['META'][key] for subtype in all_subtypes])
-        ii=ii+1
-    for key in save_keys3:
-        key1 = key+'(%s)'%(units[ii])
-        properties[key1] = np.concatenate([data[subtype]['OBJMETA'][key] for subtype in all_subtypes])
-        ii=ii+1
+    
+    ## save generated synthetic spectra
+    
+    header = [
+    {'name':'CRVAL1','value':wave[0],'comment':'Rest-Frame starting wavelength [Angstroms]'},
+    {'name':'CDELT1','value':dw,'comment':'Rest-Frame wavelength step'},
+    {'name':'LOGLAM','value':False,'comment':'Wavelength in log lambda'},
+    {'name':'RRTYPE','value':'GALAXY','comment':'Spectral type'},
+    {'name':'INSPEC','value':os.environ['DESI_BASIS_TEMPLATES'],'comment':'Vector basis'},
+    {'name':'RRVER','value':redrock.__version__,'comment':'Redrock version'},
+    {'name':'SEED','value':seed,'comment':'Input seed'},
+    {'name':'NBINPUT','value':nb,'comment':'Number of generated spectra'}
+    ]
+    out = fitsio.FITS(file_out,'rw',clobber=True)
+    props = [properties[key] for key in ['FLUX', 'SUBTYPE', 'TEMPLATEID']]
+    out.write(props,names=['FLUX','SUBTYPE','TEMPLATEID'],extname='SPECTRA', header=header)
+    final_props = [properties[key] for key in final_keys]
+    out.write(final_props,names=final_keys,extname='PROPERTIES')
+    out.close()
+
+    print(f'time took to generate {nb} galaxies: {time.time()-start} [secs]\n')
+
+
+def generate_galaxy_archetypes(parent_galaxy_filename, chi2_thresh=10**2.5, fileout=None):
+    
+    """
+    parent_galaxy_filename (string); filename of parent synthetic galaxy spectra
+    chi2_thresh (float); chi2 square threshold that will be used for setcoverpy run (default 10**2.5)
+    fileout (string); filename to save the data (.fits format)
+
+    Output:
+    ------------------------
+        Returns a fits file containing the archetype wavelength, fluxes and galaxy properties
+    
+    """
     
     start =time.time()
+
+    if not os.path.exists(parent_galaxy_filename):
+        generate_synthetic_galaxies(nb=1000, dw=0.1, file_out=parent_galaxy_filename)
+    else:
+        print(f'\nsynthetic galaxy spectra exists at {parent_galaxy_filename}, so using to generate archetype')
+
+    print(f'\n ====   Generating archetypes using {chi2_thresh} ====== \n')
+    seed = 1234
+
+    h = fits.open(parent_galaxy_filename, memmap=False)
+    hdr = h['SPECTRA'].header
+    dw = hdr['CDELT1']
+
+    ### Rest - frame wavelength definition
+    wave = np.arange(3500./(1.+1.85), 11000.+dw/2., dw)
+    
+    flux = h['SPECTRA'].data['FLUX'].astype('float64')
+    nb = flux.shape[0]
+
+    subtypes = h['SPECTRA'].data['SUBTYPE']
+    tempid  = h['SPECTRA'].data['TEMPLATEID']
+
+    properties  = Table(h['PROPERTIES'].data)
+    final_keys = list(properties.dtype.names)
     
     #chi2, amp = compute_chi2(flux)
     
@@ -177,18 +225,22 @@ def generate_archetype_galaxies(nb=100, dw=0.1, chi2_thresh=10**2.5, file_out=No
     {'name':'NBINPUT','value':nb,'comment':'Number of generated spectra'},
     {'name':'CHI2THRE','value':chi2_thresh,'comment':'Chi2 Threshold'},
     ]
-    out = fitsio.FITS(file_out,'rw',clobber=True)
+    out = fitsio.FITS(fileout,'rw',clobber=True)
     
-    final_keys = list(properties.keys())
-    props = [properties[key][iarch] for key in final_keys[3:]]
-    
-    out.write([properties['FLUX'][iarch], properties['SUBTYPE'][iarch], properties['TEMPLATEID'][iarch]],names=['ARCHETYPE','SUBTYPE', 'TEMPLATEID'],extname='ARCHETYPES', header=header)
-    out.write(props,names=final_keys[3:],extname='PROPERTIES')
+    props = [properties[key][iarch] for key in ['FLUX', 'SUBTYPE', 'TEMPLATEID']]
+    final_props = [properties[key][iarch] for key in final_keys]
+    out.write(props,names=['ARCHETYPE','SUBTYPE','TEMPLATEID'],extname='ARCHETYPES', header=header)
+    out.write(final_props,names=final_keys,extname='PROPERTIES')
     out.close()
+    print(f'\n === For chi2 = {chi2_thresh}, N (archetypes) = {iarch.size} ===== \n')
+    print(f'time took for chi2 = {chi2_thresh} to generate archetypes: {time.time()-start} [secs]\n')
 
 if __name__=='__main__':
     
-    file_out = './rrarchetype-galaxy.fits'
-    generate_archetype_galaxies(file_out=file_out)
+    main_dir = f'/global/cfs/cdirs/desi/users/abhijeet/synthetic_spectra'
+    parent_galaxy_filename = f'{main_dir}/archetypes/synthetic_galaxies.fits'
+    for pow in [2, 2.25, 2.5, 3, 3.25]:
+        chi2 = 10**pow
+        generate_galaxy_archetypes(parent_galaxy_filename, chi2_thresh=chi2, fileout=main_dir+'/rrarchetype_chi2_%d_galaxy.fits'%(int(chi2)))
 
 
